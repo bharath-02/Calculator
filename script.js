9102b8a6a911a56e3eebf4b5c8029a728c574ed0
bhara <bharathJ222000@gmail.com>
2020-10-21 21:58:47 +0530

    starting

---
+++ script.js
@@ -0,0 +1,15 @@
+var container = document.createElement('div');
+container.setAttribute('class', 'container');
+var row1 = document.createElement('div');
+row1.setAttribute('class', 'row');
+var empty_col = document.createElement('div');
+empty_col.setAttribute('class', 'col-sm-12');
+
+var row2 = document.createElement('div');
+row2.setAttribute('class', 'row');
+var col_1 = document.createElement('div');
+col_1.setAttribute('class', 'col-sm-3 col-md-3 col-lg-3');
+var col_2 = document.createElement('div');
+col_2.setAttribute('class', 'col-sm-6 col-md-6 col-lg-6');
+var col_3 = document.createElement('div');
+col_3.setAttribute('class', 'col-sm-3 col-md-3 col-lg-3');
