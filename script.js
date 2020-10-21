b6fe68dc8ac9e3e48c1d94f62740f207cdca1ce8
bhara <bharathJ222000@gmail.com>
2020-10-21 22:08:32 +0530

    adding files

--- script.js
+++ script.js
@@ -13,3 +13,17 @@ var col_2 = document.createElement('div');
 col_2.setAttribute('class', 'col-sm-6 col-md-6 col-lg-6');
 var col_3 = document.createElement('div');
 col_3.setAttribute('class', 'col-sm-3 col-md-3 col-lg-3');
+
+
+
+
+
+
+
+
+
+
+
+
+
+document.body.append(container);
