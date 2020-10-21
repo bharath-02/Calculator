06747ec4784b59a2284344f4bd0d7fc20ba20f21
bhara <bharathJ222000@gmail.com>
2020-10-21 22:23:53 +0530

    added content

--- script.js
+++ script.js
@@ -1,5 +1,6 @@
 var container = document.createElement('div');
 container.setAttribute('class', 'container');
+
 var row1 = document.createElement('div');
 row1.setAttribute('class', 'row');
 var empty_col = document.createElement('div');
@@ -14,6 +15,22 @@ col_2.setAttribute('class', 'col-sm-6 col-md-6 col-lg-6');
 var col_3 = document.createElement('div');
 col_3.setAttribute('class', 'col-sm-3 col-md-3 col-lg-3');
 
+var table = document.createElement('table');
+table.setAttribute('class', 'table table-bordered');
+
+var row_one = document.createElement('tr');
+var data_1 = document.createElement('td');
+data_1.setAttribute('colspan', '4');
+var input_field = document.createElement('input');
+input_field.setAttribute('type', 'text');
+input_field.setAttribute('class', 'form-control');
+input_field.id = 'input-value';
+
+var row_two = document.createElement('tr');
+var data_2 = document.createElement('td');
+var data_3 = document.createElement('td');
+var data_4 = document.createElement('td');
+var data_5 = document.createElement('td');
 
 
 
@@ -22,8 +39,11 @@ col_3.setAttribute('class', 'col-sm-3 col-md-3 col-lg-3');
 
 
 
-
-
-
-
+data_1.append(input_field);
+row_one.append(data_1);
+table.append(row_one);
+col_2.append(table);
+row1.append(empty_col);
+row2.append(col_1, col_2, col_3);
+container.append(row1, row2);
 document.body.append(container);
